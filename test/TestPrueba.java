164146bf02fc4ae9310621d4425a8481e3c3ec14
gianbelinche <gianbelinche@hotmail.com.ar>
2019-06-01 20:36:49 -0300

    MOdifico build

---
+++ test/TestPrueba.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class PruebaTest {
+    @Test
+    public void prueba(){
+        public int a = 5;
+        assertEquals(0,a);
+    }
+
+}
