eb9ede8bda53909c81f8d9a91ac6f2b4f769f810
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2019-06-10 17:21:47 -0300

    Por alguna razon no me añade todos los archivos

--- test/Modelo/CrafteadorTest.java
+++ test/Modelo/CrafteadorTest.java
@@ -7,18 +7,14 @@ public class CrafteadorTest {
 
     @Test
     public void testCorrectoCrafteoDeHachaDeMadera(){
-        /*
+
         MesaDeCrafteo mesa = new MesaDeCrafteo();
-        Madera madera1 = new Madera();
-        mesa.posicionar(0,0,madera1);
-        Madera madera2 = new Madera();
-        mesa.posicionar(0,1,madera2);
-        Madera madera3 = new Madera();
-        mesa.posicionar(1,0,madera3);
-        Madera madera4 = new Madera();
-        mesa.posicionar(1,1,madera4);
-        Madera madera5 = new Madera();
-        mesa.posicionar(2,1,madera5);
+        Madera madera = new Madera();
+        mesa.posicionar(0,0,madera);
+        mesa.posicionar(0,1,madera);
+        mesa.posicionar(1,0,madera);
+        mesa.posicionar(1,1,madera);
+        mesa.posicionar(2,1,madera);
 
         Crafteador crafteador = new Crafteador();
 
@@ -26,6 +22,48 @@ public class CrafteadorTest {
         int fuerzaDelHacha = 2;
         int fuerza = hacha.fuerza();
         assertEquals(fuerzaDelHacha,fuerza);
-        */
+
+    }
+
+    @Test
+    public void testCorrectoCrafteoDeHachaDePiedra(){
+
+        MesaDeCrafteo mesa = new MesaDeCrafteo();
+        Madera madera = new Madera();
+        Piedra piedra = new Piedra();
+        mesa.posicionar(0,0,piedra);
+        mesa.posicionar(0,1,piedra);
+        mesa.posicionar(1,0,piedra);
+        mesa.posicionar(1,1,madera);
+        mesa.posicionar(2,1,madera);
+
+        Crafteador crafteador = new Crafteador();
+
+        Herramienta hacha = crafteador.craftear(mesa);
+        int fuerzaDelHacha = 5;
+        int fuerza = hacha.fuerza();
+        assertEquals(fuerzaDelHacha,fuerza);
+
+    }
+
+    @Test
+    public void testCorrectoCrafteoDeHachaDeMetal(){
+
+        MesaDeCrafteo mesa = new MesaDeCrafteo();
+        Madera madera = new Madera();
+        Metal metal = new Metal();
+        mesa.posicionar(0,0,metal);
+        mesa.posicionar(0,1,metal);
+        mesa.posicionar(1,0,metal);
+        mesa.posicionar(1,1,madera);
+        mesa.posicionar(2,1,madera);
+
+        Crafteador crafteador = new Crafteador();
+
+        Herramienta hacha = crafteador.craftear(mesa);
+        int fuerzaDelHacha = 10;
+        int fuerza = hacha.fuerza();
+        assertEquals(fuerzaDelHacha,fuerza);
+
     }
 }
--- test/Modelo/MesaDeCrafteoTest.java
+++ test/Modelo/MesaDeCrafteoTest.java
@@ -1,6 +1,9 @@
 package Modelo;
 
 import org.junit.Test;
+
+import java.util.Objects;
+
 import static org.junit.Assert.*;
 
 public class MesaDeCrafteoTest {
@@ -8,8 +11,8 @@ public class MesaDeCrafteoTest {
     @Test
     public void testCorrectaInicializacionDeMesaDeCrafteo(){
         MesaDeCrafteo mesa = new MesaDeCrafteo();
-        Material [][] semilla = mesa.obtenerSemilla();
-        assertEquals(null,semilla[0][0]);
+
+        assertNull(mesa.enPosicion(0,0));
     }
 
     @Test
@@ -17,7 +20,90 @@ public class MesaDeCrafteoTest {
         MesaDeCrafteo mesa = new MesaDeCrafteo();
         Madera madera = new Madera();
         mesa.posicionar(0,0,madera);
-        Material [][] semilla = mesa.obtenerSemilla();
-        assertEquals(madera,semilla[0][0]);
+        assertEquals(madera,mesa.enPosicion(0,0));
     }
+
+    @Test
+    public void testCorrectoPosicionamientoDeMaterialEnLaPosicion01(){
+        MesaDeCrafteo mesa = new MesaDeCrafteo();
+        Madera madera = new Madera();
+        mesa.posicionar(0,1,madera);
+        assertEquals(madera,mesa.enPosicion(0,1));
+    }
+
+    @Test
+    public void testCorrectoPosicionamientoDeMaterialEnLaPosicion02(){
+        MesaDeCrafteo mesa = new MesaDeCrafteo();
+        Piedra piedra = new Piedra();
+        mesa.posicionar(0,2,piedra);
+        assertEquals(piedra,mesa.enPosicion(0,2));
+    }
+
+    @Test
+    public void testCorrectoPosicionamientoDeMaterialEnLaPosicion10(){
+        MesaDeCrafteo mesa = new MesaDeCrafteo();
+        Piedra piedra = new Piedra();
+        mesa.posicionar(1,0,piedra);
+        assertEquals(piedra,mesa.enPosicion(1,0));
+    }
+
+    @Test
+    public void testCorrectoPosicionamientoDeMaterialEnLaPosicion11(){
+        MesaDeCrafteo mesa = new MesaDeCrafteo();
+        Madera madera = new Madera();
+        mesa.posicionar(1,1,madera);
+        assertEquals(madera,mesa.enPosicion(1,1));
+    }
+
+    @Test
+    public void testCorrectoPosicionamientoDeMaterialEnLaPosicion12(){
+        MesaDeCrafteo mesa = new MesaDeCrafteo();
+        Metal metal = new Metal();
+        mesa.posicionar(1,2,metal);
+        assertEquals(metal,mesa.enPosicion(1,2));
+    }
+
+    @Test
+    public void testCorrectoPosicionamientoDeMaterialEnLaPosicion20(){
+        MesaDeCrafteo mesa = new MesaDeCrafteo();
+        Madera madera = new Madera();
+        mesa.posicionar(2,0,madera);
+        assertEquals(madera,mesa.enPosicion(2,0));
+    }
+
+    @Test
+    public void testCorrectoPosicionamientoDeMaterialEnLaPosicion21(){
+        MesaDeCrafteo mesa = new MesaDeCrafteo();
+        Metal metal = new Metal();
+        mesa.posicionar(2,1,metal);
+        assertEquals(metal,mesa.enPosicion(2,1));
+    }
+
+    @Test
+    public void testCorrectoPosicionamientoDeMaterialEnLaPosicion22(){
+        MesaDeCrafteo mesa = new MesaDeCrafteo();
+        Madera madera = new Madera();
+        mesa.posicionar(2,2,madera);
+        assertEquals(madera,mesa.enPosicion(2,2));
+    }
+
+    @Test
+    public void testDosMesasDeCrafteoVaciasSonIguales(){
+        MesaDeCrafteo mesa1 = new MesaDeCrafteo();
+        MesaDeCrafteo mesa2 = new MesaDeCrafteo();
+        assert(mesa1.equals(mesa2));
+    }
+
+    @Test
+    public void testDosMesasConMaderaEnLaPrimerPosicionSonIguales(){
+        MesaDeCrafteo mesa1 = new MesaDeCrafteo();
+        MesaDeCrafteo mesa2 = new MesaDeCrafteo();
+        Madera madera1 = new Madera();
+        Madera madera2 = new Madera();
+        mesa1.posicionar(0,0,madera1);
+        mesa2.posicionar(0,0,madera2);
+        assert(mesa1.equals(mesa2));
+    }
+
+
 }
