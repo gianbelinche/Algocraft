317cab86c2a2d5dd29139ae2dbea0f8b3c3a0caa
Julián <julianv6004@gmail.com>
2019-06-14 00:47:23 -0300

    Pruebas de vista, prueba comiteando desde IntelliJ

---
+++ src/Vista/ContenedorInventario.java
@@ -0,0 +1,40 @@
+package Vista;
+
+import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+public class ContenedorInventario extends VBox {
+    Stage stage;
+    public ContenedorInventario(Stage stage){
+
+        super();
+        this.stage = stage;
+        this.setAlignment(Pos.CENTER);
+        this.setSpacing(20);
+        this.setPadding(new Insets(25));
+        Image imagen = new Image("file:src/Vista/Imagenes/Fondo_con_inventario.png");
+        BackgroundImage imagenDeInventario = new BackgroundImage(imagen, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        this.setBackground(new Background(imagenDeInventario));
+
+        Button botonCraftear = new Button();
+        botonCraftear.setText("Craftear");
+
+        this.getChildren().addAll(botonCraftear);
+    }
+
+}
--- test/Modelo/MaderaTest.java
+++ test/Modelo/MaderaTest.java
@@ -2,6 +2,7 @@ package Modelo;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MaderaTest {
