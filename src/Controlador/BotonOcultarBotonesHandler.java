4c49aee17b563c88a830a3c28ada5c37888014fe
Guido Botta <gbotta@fi.uba.ar>
2019-06-16 02:51:28 -0300

    Funciona Ocultar/Mostrar Botones

---
+++ src/Controlador/BotonOcultarBotonesHandler.java
@@ -0,0 +1,32 @@
+package Controlador;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+public class BotonOcultarBotonesHandler implements EventHandler<ActionEvent> {
+
+    Button button;
+    HBox caja;
+    private int contador = 0;
+
+    public BotonOcultarBotonesHandler(Button button, HBox cajaCompleta){
+        this.button = button;
+        this.caja = cajaCompleta;
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if ((contador%2) == 0) {
+            button.setText("Mostrar Botones");
+            caja.setVisible(false);
+        }
+        else {
+            button.setText("Ocultar Botones");
+            caja.setVisible(true);
+        }
+        contador += 1;
+    }
+
+}
--- src/Vista/BotonOcultarBotones.java
+++ src/Vista/BotonOcultarBotones.java
@@ -1,13 +1,18 @@
 package Vista;
 
+import Controlador.BotonOcultarBotonesHandler;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class BotonOcultarBotones extends Button {
 
-    public BotonOcultarBotones(){
+    public BotonOcultarBotones(HBox cajaCompleta){
         this.setText("Ocultar Botones");
         this.setTranslateX(15);
         this.setTranslateY(20);
+
+        BotonOcultarBotonesHandler botonOcultarBotonesHandler = new BotonOcultarBotonesHandler(this, cajaCompleta);
+        this.setOnAction(botonOcultarBotonesHandler);
     }
 
 }
