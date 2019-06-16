a99e83c8b717817c4db85a0ce96a5c89f71e138f
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2019-06-16 02:47:55 -0300

    Boton mover arriba

--- src/Vista/BotonMoverArriba.java
+++ src/Vista/BotonMoverArriba.java
@@ -1,12 +1,26 @@
 package Vista;
 
+import Modelo.Jugador;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class BotonMoverArriba extends Button {
+    Jugador jugador;
+    Ventana ventana;
 
-    public BotonMoverArriba(Stage stage){
+    public BotonMoverArriba(Ventana ventana, Jugador jugador) {
         this.setText("^");
+        this.jugador = jugador;
+        this.ventana = ventana;
+        }
+
+    public Jugador obtenerJugador(){
+        return jugador;
     }
+
+    public Ventana obtenerVentana(){
+        return ventana;
+    }
+
 
 }
