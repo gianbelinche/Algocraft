6ce86025a50747180d8fbb6e563158df6d27fe91
guidobotta <gbotta@fi.uba.ar>
2019-06-23 21:00:54 -0300

    No se por qué no se commiteo esto

--- src/Controlador/BotonesHandlers/BotonEntrarSalirInventarioHandler.java
+++ src/Controlador/BotonesHandlers/BotonEntrarSalirInventarioHandler.java
@@ -6,20 +6,16 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.layout.VBox;
 
-public class BotonEntrarInventarioHandler implements EventHandler<ActionEvent> {
+public class BotonEntrarSalirInventarioHandler implements EventHandler<ActionEvent> {
 
         private InventarioLayout inventario;
-        private VBox botones;
 
-        public BotonEntrarInventarioHandler(VBox contenedorBotones, InventarioLayout unInventario){
+        public BotonEntrarSalirInventarioHandler(InventarioLayout unInventario){
             this.inventario = unInventario;
-            this.botones = contenedorBotones;
         }
         @Override
         public void handle(ActionEvent event){
-
-            inventario.setVisible(true);
-            //botones.setVisible(false);
+            inventario.setVisible(!inventario.isVisible());
         }
 
 
