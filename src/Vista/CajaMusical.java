2ba3d96003b9f3ebdd05c0ddd9c55dfbd4f27f7c
FrancoBar <francobarreneche@hotmail.com.ar>
2019-06-27 10:37:57 -0300

    No funcionó, revierto a la versión más estable.

--- src/Vista/CajaMusical.java
+++ src/Vista/CajaMusical.java
@@ -6,9 +6,7 @@ import javafx.scene.media.MediaPlayer;
 import java.io.File;
 
 public class CajaMusical {
-
-
-
+    
     public static void reproducirMusicaDeFondo(){
         String musicaFondo = "src/Vista/Sonidos/C418  - Sweden - Minecraft Volume Alpha.mp3";
         Media melodia = new Media(new File(musicaFondo).toURI().toString());
