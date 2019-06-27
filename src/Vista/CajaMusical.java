ddeba4825b71c03de0ba10c586c171b0c7941aed
FrancoBar <francobarreneche@hotmail.com.ar>
2019-06-27 10:13:54 -0300

    Otro intento de arreglar el Sonido.

--- src/Vista/CajaMusical.java
+++ src/Vista/CajaMusical.java
@@ -7,13 +7,15 @@ import java.io.File;
 
 public class CajaMusical {
 
+    static MediaPlayer  mediaPlayerSonido;
+    static MediaPlayer mediaPlayerFondo;
 
 
     public static void reproducirMusicaDeFondo(){
         String musicaFondo = "src/Vista/Sonidos/C418  - Sweden - Minecraft Volume Alpha.mp3";
         Media melodia = new Media(new File(musicaFondo).toURI().toString());
-        MediaPlayer mediaPlayer = new MediaPlayer(melodia);
-        mediaPlayer.play();
+        mediaPlayerFondo = new MediaPlayer(melodia);
+        mediaPlayerFondo.play();
     }
 
     public static void reproducirMusicaDeHacha(){
@@ -27,8 +29,9 @@ public class CajaMusical {
     public static void reproducirMusicaGanar(){
         String musicaGanar = "src/Vista/Sonidos/Sonido ganar.wav";
         Media melodia = new Media(new File(musicaGanar).toURI().toString());
-        MediaPlayer mediaPlayer = new MediaPlayer(melodia);
-        mediaPlayer.setVolume(0.5);
-        mediaPlayer.play();
+        mediaPlayerSonido.stop();
+        mediaPlayerSonido = new MediaPlayer(melodia);
+        mediaPlayerSonido.setVolume(0.5);
+        mediaPlayerSonido.play();
     }
 }
